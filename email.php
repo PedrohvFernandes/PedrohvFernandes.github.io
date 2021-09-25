187862f428ad24ac76f8abe72f24dbf570a9cd7e
Pedro Henrique Vieira Fernandes <Pedrohv20fernandes@gmail.com>
2021-09-25 19:14:59 -0300

    PHP

--- email.php
+++ email.php
@@ -3,17 +3,18 @@ if(isset($_POST['email']) && !empty($_POST['email'])){
 
 
 $nome = addcslashes($_POST['nome']);
-$email=addcslashes($_POST['email']);
-$mensagem=addcslashes($_POST['descrition']);
-$file=addcslashes($_POST['arquivo']);
+$email = addcslashes($_POST['email']);
+$mensagem = addcslashes($_POST['descrition']);
+$file = addcslashes($_POST['arquivo']);
 
-$to = "pedrohv20fernandes@gmail.com"
-$subject = "vagas para mim do meu portfolio"
+$to = "pedrohv20fernandes@gmail.com";
+$subject = "vagas para mim do meu portfolio";
 $body = "Nome: ".$nome."\r\n".
         "Email: ".$email."\r\n".
         "Mensagem: ".$mensagem."\r\n".
         "File: ".$file."\r\n";
-$header ="From: pedrohv20fernandes@gmail.com"."\r\n"."Reply-To:".$email."\r\n".
+$header ="From:pedrohv20fernandes@gmail.com"."\r\n".
+"Reply-To:".$email."\r\n".
 "X=Mailer:PHP/".phpversion();
 
 

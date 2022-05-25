37b8eb7716224ae4ceb4bfc0524f5a56c123d7a8
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2022-05-25 14:55:40 -0300

    Adicionando efeito no scroll e nos links do menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,44 +5,46 @@ function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
 
-  // activateMenuAtCurrentSection(home);
-  // activateMenuAtCurrentSection(services);
-  // activateMenuAtCurrentSection(about);
-  // activateMenuAtCurrentSection(contact);
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(experience);
+  activateMenuAtCurrentSection(project);
+  activateMenuAtCurrentSection(technologies);
+  activateMenuAtCurrentSection(contact);
 }
 
-// function activateMenuAtCurrentSection(section) {
+function activateMenuAtCurrentSection(section) {
 
-//   const targetLine = scrollY + innerHeight / 2;
-//   console.log('Linha alvo: ', targetLine);
+  const targetLine = scrollY + innerHeight / 2;
+  console.log('Linha alvo: ', targetLine);
 
-//   const sectionTop = section.offsetTop
-//   console.log('Topo da seção: ', sectionTop)
+  const sectionTop = section.offsetTop
+  console.log('Topo da seção: ', sectionTop)
 
-//   const sectionHeight = section.offsetHeight
-//   console.log('Altura da seção: ', sectionHeight)
+  const sectionHeight = section.offsetHeight
+  console.log('Altura da seção: ', sectionHeight)
 
-//   const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
-//   console.log('O topo da seção chegou ou passou da linha ? ', sectionTopReachOrPassedTargetLine)
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+  console.log('O topo da seção chegou ou passou da linha ? ', sectionTopReachOrPassedTargetLine)
 
-//   const sectionEndsAt = sectionTop + sectionHeight
-//   console.log('final da seçao que é o topo + altura da seçao: ', sectionEndsAt)
+  const sectionEndsAt = sectionTop + sectionHeight
+  console.log('final da seçao que é o topo + altura da seçao: ', sectionEndsAt)
 
-//   const sectionEndPassedTargetLine =  sectionEndsAt <= targetLine
-//   console.log('O fundo da seção passou da linha ? ', sectionEndPassedTargetLine)
+  const sectionEndPassedTargetLine =  sectionEndsAt <= targetLine
+  console.log('O fundo da seção passou da linha ? ', sectionEndPassedTargetLine)
 
-//   const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
-//   console.log('Limite da seção ? ', sectionBoundaries)
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+  console.log('Limite da seção ? ', sectionBoundaries)
 
-//   const sectionId = section.getAttribute('id')
-//   const menuElement = document
-//   .querySelector(`.menu a[href*=${sectionId}]`)
+  const sectionId = section.getAttribute('id')
+  const menuElement = document
+  .querySelector(`.menu a[href*=${sectionId}]`)
 
-//   menuElement.classList.remove('active')
-//   if(sectionBoundaries){
-//     menuElement.classList.add('active')
-//   }
-// }
+  menuElement.classList.remove('active')
+  if(sectionBoundaries){
+    menuElement.classList.add('active')
+  }
+}
 
 function showNavOnScroll() {
   if (scrollY >= 64) {
@@ -68,28 +70,15 @@ function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
-// ScrollReveal({
-//   origin: 'top',
-//   distance: '30px',
-//   duration: 700,
-//   reset: true
-// }).reveal(`
-//   #home,
-//   #home header,
-//   #home .content,
-//   #home img,
-//   #home .stats,
-//   #services,
-//   #services header,
-//   #services .card,
-//   #about,
-//   #about header,
-//   #about .content,
-//   #contact,
-//   #contact header,
-//   #contact .content,
-//   #contact img,
-//   #footer,
-//   #footer a,
-//   #footer p,
-//   #footer .social-links`)
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+}).reveal(`
+  #home,
+  #about,
+  #experience,
+  #project,
+  #technologies,
+  #contact`)

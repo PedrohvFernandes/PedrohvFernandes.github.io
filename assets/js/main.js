bbfdbed67caa64052779995c8766a28b1ab9a5da
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2022-05-26 18:22:24 -0300

    About me feito

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,9 +96,49 @@ function typeWriter(element) {
   textArray.forEach((letter, i) => {
     // Cada letra tem um tempo de delay, que é o tempo que ela ficará escrita
     // Pra cada letra ele adiciona uma apos a outra
-    setTimeout(() => element.innerHTML += letter, 130 * i) // a primeira letra é 0 -> 75 * 0 = 0, a segunda é 75 * 1 = 75, a terceira é 75 * 2 = 150, etc
-  });
+    setTimeout(() => (element.innerHTML += letter), 90 * i) // a primeira letra é 0 -> 75 * 0 = 0, a segunda é 75 * 1 = 75, a terceira é 75 * 2 = 150, etc
+  })
 }
 
 const spanTitle = document.querySelector('#spanTitle')
 typeWriter(spanTitle)
+
+// function minhaIdade() {
+//   let data = new Date()
+//   let ano = data.getFullYear()
+//   let nascimento = 2001;
+//   let idade = ano - nascimento
+//   document.getElementById('anos').innerHTML = idade
+// }
+// minhaIdade()
+
+function calculaIdade(dataNasc) {
+  // Pegando uma data nova
+  let dataAtual = new Date()
+  // Pegando o ano atual
+  let anoAtual = dataAtual.getFullYear()
+  // Pegando a data de nascimento e criando um vetor de tres indices(dia, mes e ano)
+  let anoNascParts = dataNasc.split('/')
+  // Capturando a data de nascimento em cada variavel
+  let diaNasc = anoNascParts[0]
+  let mesNasc = anoNascParts[1]
+  let anoNasc = anoNascParts[2]
+  // Idade da pessoa
+  let idade = anoAtual - anoNasc
+  // A gente pega o mes + 1 porque o date pega mes anterior ao atual
+  let mesAtual = dataAtual.getMonth() + 1
+  //Se mes atual for menor que o nascimento, nao fez aniversario ainda;
+  if (mesAtual < mesNasc) {
+    idade--
+  } else {
+    //Se estiver no mes do nascimento, verificar o dia
+    if (mesAtual === mesNasc) {
+      if (new Date().getDate() < diaNasc) {
+        //Se a data atual for menor que o dia de nascimento ele ainda nao fez aniversario
+        idade--
+      }
+    }
+  }
+  document.getElementById('anos').innerHTML = idade
+}
+calculaIdade('05/12/2001')
